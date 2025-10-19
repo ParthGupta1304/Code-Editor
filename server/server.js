8a29f9c4930ab002a04820b9b18a3fcd34ba3ad6
Parth Gupta <guptaparth1304@gmail.com>
2025-10-19 14:18:27 +0530

    updates express

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-
+import express from "express";
+const app = express();
 dotenv.config();
-
+app.use(express.json());
 const MONGO_URI = process.env.MONGODB_URI;
 if (!MONGO_URI) {
   console.error(
