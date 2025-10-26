1b9d8158d6a4e4c841a6e3d168e53cd41581471b
Parth Gupta <guptaparth1304@gmail.com>
2025-10-26 13:56:05 +0530

    Fix: Comment out missing pages for deployment

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Editor from "./pages/Editor.jsx";
-import Login from "./pages/Login.jsx";
-import SignUp from "./pages/SignUp.jsx";
-import About from "./pages/About.jsx";
+// import Login from "./pages/Login.jsx";
+// import SignUp from "./pages/SignUp.jsx";
+// import About from "./pages/About.jsx";
 
 const App = () => {
   return (
@@ -12,9 +12,9 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/editor" element={<Editor />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/about" element={<About />} />
+          {/* <Route path="/login" element={<Login />} /> */}
+          {/* <Route path="/signup" element={<SignUp />} /> */}
+          {/* <Route path="/about" element={<About />} /> */}
         </Routes>
       </div>
     </Router>
